7fbc3c5a331956a8bb227e0eeb2f645a2502eb34
Jaeren William Tredway <jwilliamtredway@gmail.com>
2020-04-24 10:44:27 -0600

    Made formatting edits.

--- src/EuclidsAlgorithm.java
+++ src/EuclidsAlgorithm.java
@@ -7,12 +7,11 @@ import java.util.Scanner;
  * This implements the extended Euclid's Algorithm to get gcd(a, b) and the
  * linear combination gcd(a, b) = sa + tb as in Bezout's Identity
  *
- * scanner input: two positive integers a and b
+ * scanner user input: two positive integers a and b
  * output: from the Euclidian Algorithm --> gcd(a, b)
- * output: as per Bezout's Identity (gcd(a, b) = sa + tb) --> s, t
+ * output: as per Bezout's Identity --> gcd(a, b) = sa + tb
  *
- * to complile in command line: javac EuclidsAlgorithm.java from inside the
- * src directory
+ * to complile in command line: javac EuclidsAlgorithm.java
  *
  * to run program: java EuclidsAlgorithm
  */
@@ -20,12 +19,11 @@ public class EuclidsAlgorithm {
 
     //CLASS VARIABLES:
     private static Scanner scanner = new Scanner(System.in);
-    static int a = 0; //first input number: the SMALLER ONE
-    static int b = 0; //first input number: the LARGER ONE
+    static int a = 0; //first user-input number
+    static int b = 0; //second user-input number
     static int s = 0; //the Bezout's coefficient to a
     static int t = 0; //the Bezout's coefficient to b
     static int d = 0; //the value of gcd(a , b)
-
 
     //get user input for a and b:
     static void getInput () {
@@ -37,33 +35,36 @@ public class EuclidsAlgorithm {
         scanner.nextLine();
     }
 
-    //build an array to store [d, s, t]
+    //build an array to store [d, s, t] for each step
     //d = gcd(a, b) = sa + tb
-    static int[] gcd(int a, int b) {
-        //exit condition:
-        if (b == 0)
+    private static int[] gcd(int a, int b) {
+    //exit condition:
+        if (b == 0) {
             return new int[] { a, 1, 0 };
-        //run the recursion:
+        }
+    //run the recursions until you get to the exit condition:
         int[] store = gcd(b, a % b);
+    //assign the final values to the class variables from each recursion step:
         d = store[0];
         s = store[2];
         t = store[1] - (a / b) * store[2];
         return new int[] { d, s, t };
     }
 
+    //there are no command line args:
     public static void main(String[] args) {
     //get the user input:
         getInput();
     //run the algorithm:
-        int[] store = gcd(a, b);
+        gcd(a, b);
     //print the output:
         System.out.println("Greatest Common Denominator from Euclidian " +
                 "Algorithm:");
-        System.out.println("gcd(" + a + ", " + b + ") = " + store[0]);
+        System.out.println("gcd(" + a + ", " + b + ") = " + d);
         System.out.println("Linear Combination as in Bezout's Identity:");
         System.out.println("s(a) + t(b) = gcd(a, b)");
-        System.out.println(store[1] + "(" + a + ") + " + store[2] +
-                "(" + b + ") = " + store[0]);
+        System.out.println(s + "(" + a + ") + " + t +
+                "(" + b + ") = " + d);
     }//END main()
 }//END class EuclidsAlgorithm
 
